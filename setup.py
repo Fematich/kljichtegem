826379bc9e68bb7c92e4b3d409eeab5f02df5060
Fematich <matthiasfeys@gmail.com>
2014-09-17 22:36:44 +0200

    Initial deployment of this app to the web

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='KLJIchtegemSite',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='website voor KLJ Ichtegem',
+      author='Matthias Feys',
+      author_email='matthiasfeys@hotmail.com',
       url='https://www.python.org/community/sigs/current/distutils-sig',
       install_requires=['Flask>=0.7.2', 'MarkupSafe'],
      )
