17114b88a91ec02600b013976742ff45a61e128a
Fematich <matthiasfeys@gmail.com>
2014-09-17 22:45:23 +0200

    templates

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,5 +1,20 @@
+from flask import Flask, flash, render_template, g
+
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/activiteiten')
+def activiteiten():
+    return render_template('activiteiten.html')
+    
+@app.route('/bestuur')
+def bestuur():
+    return render_template('bestuur.html')
+
+@app.route('/nachtvantwodkapje')
+def feestweekend():
+    return render_template('feestweekend.html')
