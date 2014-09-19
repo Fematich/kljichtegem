28c808cc99a6d92bd93e5414dd1f36ecbb625fe4
Fematich <matthiasfeys@gmail.com>
2014-09-19 12:30:29 +0200

    postgresql3

--- wsgi/app/__init__.py
+++ wsgi/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask  
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.security import Security, SQLAlchemyUserDatastore
+#from flask.ext.security import Security, SQLAlchemyUserDatastore
 
 app = Flask(__name__)  
 app.config.from_object('config')
@@ -12,10 +12,6 @@ db = SQLAlchemy(app)
 
 
 
-from models import User, Role
-# flask-security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
 
 from views import *
 
