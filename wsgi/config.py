5e3d4249a636a2f94912707c288891468432a1c8
Matthias Feys <matthiasfeys@gmail.com>
2017-09-29 23:26:24 +0200

    update postgresql

--- wsgi/config.py
+++ wsgi/config.py
@@ -18,9 +18,3 @@ SECURITY_POST_LOGIN_VIEW='/admin'
 EVENTS_PER_PAGE=6
 
 DEBUG = True
-
-if 'OPENSHIFT_POSTGRESQL_DB_HOST' in os.environ:
-    pg_url = 'postgresql://%s:%s/%s' % (
-        os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
-        os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],
-        os.environ['OPENSHIFT_APP_NAME'])
