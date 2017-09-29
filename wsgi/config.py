fc5b7cc59fdbd4c435898b265053c18ac0982f91
Matthias Feys <matthiasfeys@gmail.com>
2017-09-29 23:23:28 +0200

    update postgresql

--- wsgi/config.py
+++ wsgi/config.py
@@ -2,7 +2,7 @@ import os
 
 ################################################################
 #################### SQLAlchemy settings #######################
-SQLALCHEMY_DATABASE_URI = os.environ['OPENSHIFT_POSTGRESQL_DB_URL']
+SQLALCHEMY_DATABASE_URI = os.environ['postgresql_SERVICE_HOST']
 SQLALCHEMY_ECHO = False
 
 ################################################################
@@ -18,3 +18,9 @@ SECURITY_POST_LOGIN_VIEW='/admin'
 EVENTS_PER_PAGE=6
 
 DEBUG = True
+
+if 'OPENSHIFT_POSTGRESQL_DB_HOST' in os.environ:
+    pg_url = 'postgresql://%s:%s/%s' % (
+        os.environ['OPENSHIFT_POSTGRESQL_DB_HOST'],
+        os.environ['OPENSHIFT_POSTGRESQL_DB_PORT'],
+        os.environ['OPENSHIFT_APP_NAME'])
