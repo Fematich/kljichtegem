dd3c1db27c26fa593e6d9b5a1768e16fba030af6
Matthias Feys <matthiasfeys@gmail.com>
2017-09-29 23:42:48 +0200

    update logging

--- wsgi/config.py
+++ wsgi/config.py
@@ -1,5 +1,7 @@
 import os
+import logging
 
+logging.info(str(os.environ))
 ################################################################
 #################### SQLAlchemy settings #######################
 SQLALCHEMY_DATABASE_URI = os.environ['postgresql_SERVICE_HOST']
@@ -12,9 +14,9 @@ SECRET_KEY = 'you-will-never-guess'
 
 ################################################################
 ######################## login settings ########################
-SECURITY_POST_LOGIN_VIEW='/admin'
+SECURITY_POST_LOGIN_VIEW = '/admin'
 
 
-EVENTS_PER_PAGE=6
+EVENTS_PER_PAGE = 6
 
 DEBUG = True
